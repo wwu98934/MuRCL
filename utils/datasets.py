ff88bff783711027da7c30e1fe2438116b719dab
98934 <39588409+98934@users.noreply.github.com>
2021-12-10 19:43:22 +0800

    add some comments

--- utils/datasets.py
+++ utils/datasets.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from typing import Tuple, Iterable, Dict, Union, List
 
 import torch
 from torch.utils.data.dataset import Dataset
@@ -9,8 +10,26 @@ from utils.general import load_json
 
 
 class WSIDataset(Dataset):
-    def __init__(self, data_csv, indices=None, num_sample_patches=None, fixed_size=False, shuffle=False,
-                 patch_random=False, preload=True):
+    """Basic WSI Dataset, which can obtain the features of each patch of WSIs."""
+
+    def __init__(self,
+                 data_csv: Union[str, Path],
+                 indices: Iterable[str] = None,
+                 num_sample_patches: int = None,
+                 fixed_size: bool = False,
+                 shuffle: bool = False,
+                 patch_random: bool = False,
+                 preload: bool = True) -> None:
+        """Initialization constructor.
+
+        :param str or Path data_csv: A CSV file's filepath for organization WSI data, as detailed in our README
+        :param Iterable[str] indices: A list containing the specified `case_id`, if None, fetching all `case_id` in the `data_csv` file
+        :param int num_sample_patches: The number of sampled patches, if None, the value is the number of all patches
+        :param bool fixed_size: If True, the size of the number of patches is fixed
+        :param bool shuffle: If True, shuffle the order of all WSIs
+        :param bool patch_random: If True, shuffle the order of patches within a WSI during reading this dataset
+        :param bool preload: If True, all feature files are loaded at initialization
+        """
         super(WSIDataset, self).__init__()
         self.data_csv = data_csv
         self.indices = indices
@@ -30,10 +49,10 @@ class WSIDataset(Dataset):
         if self.preload:
             self.patch_features = self.load_patch_features()
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.samples)
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor, str]:
         case_id = self.indices[index]
 
         if self.preload:
@@ -50,10 +69,12 @@ class WSIDataset(Dataset):
         label = torch.tensor(label, dtype=torch.long)
         return patch_feature, label, case_id
 
-    def shuffle(self):
+    def shuffle(self) -> None:
+        """Shuffle the order of WSIs. """
         random.shuffle(self.indices)
 
     def process_data(self):
+        """Load the `data_csv` file by `indices`. """
         data_csv = pd.read_csv(self.data_csv)
         data_csv.set_index(keys='case_id', inplace=True)
         if self.indices is not None:
@@ -62,13 +83,15 @@ class WSIDataset(Dataset):
             samples = data_csv
         return samples
 
-    def load_patch_features(self):
+    def load_patch_features(self) -> Dict[str, np.ndarray]:
+        """Load the all the patch features of all WSIs. """
         patch_features = {}
         for case_id in self.indices:
             patch_features[case_id] = np.load(self.samples.at[case_id, 'features_filepath'])['img_features']
         return patch_features
 
-    def sample_feat(self, patch_feature):
+    def sample_feat(self, patch_feature: np.ndarray) -> np.ndarray:
+        """Sample features by `num_sample_patches`. """
         num_patches = patch_feature.shape[0]
         if self.num_sample_patches is not None and num_patches > self.num_sample_patches:
             sample_indices = np.random.choice(num_patches, size=self.num_sample_patches, replace=False)
@@ -78,7 +101,8 @@ class WSIDataset(Dataset):
             np.random.shuffle(patch_feature)
         return patch_feature
 
-    def fix_size(self, patch_feature):
+    def fix_size(self, patch_feature: np.ndarray) -> np.ndarray:
+        """Fixed the shape of each WSI feature. """
         if patch_feature.shape[0] < self.num_sample_patches:
             margin = self.num_sample_patches - patch_feature.shape[0]
             feat_pad = np.zeros(shape=(margin, self.patch_dim))
@@ -89,16 +113,37 @@ class WSIDataset(Dataset):
 
 
 class WSIWithCluster(WSIDataset):
-    def __init__(self, data_csv, indices=None, num_sample_patches=None, fixed_size=False, shuffle=False,
-                 patch_random=False, preload=True):
+    """A WSI Dataset with its cluster result"""
+
+    def __init__(self,
+                 data_csv: Union[str, Path],
+                 indices: Iterable[str] = None,
+                 num_sample_patches: int = None,
+                 fixed_size: bool = False,
+                 shuffle: bool = False,
+                 patch_random: bool = False,
+                 preload: bool = True) -> None:
+        """Initialization constructor.
+
+        :param str or Path data_csv: A CSV file's filepath for organization WSI data, the end of the filename must provide the number of clusters, as detailed in our README
+        :param Iterable[str] indices: A list containing the specified `case_id`, if None, fetching all `case_id` in the `data_csv` file
+        :param int num_sample_patches: The number of sampled patches, if None, the value is the number of all patches
+        :param bool fixed_size: If True, the size of the number of patches is fixed
+        :param bool shuffle: If True, shuffle the order of all WSIs
+        :param bool patch_random: If True, shuffle the order of patches within a WSI during reading this dataset
+        :param bool preload: If True, all feature files are loaded at initialization
+        """
         super(WSIWithCluster, self).__init__(data_csv, indices, num_sample_patches, fixed_size, shuffle, patch_random,
                                              preload)
+        # The filename of `data_csv` must provide the number of clusters at the end.
+        # eg. camelyon16_10.csv, the 10 indicates the number of clusters.
         self.num_clusters = int(Path(data_csv).stem.split('_')[-1])
 
         if self.preload:
             self.cluster_indices = self.load_cluster_indices()
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, List[List[int]], torch.Tensor, str]:
+        """Return the WSI features, cluster indices, label, and the case_id. """
         case_id = self.indices[index]
 
         if self.preload:
@@ -113,7 +158,7 @@ class WSIWithCluster(WSIDataset):
         label = torch.tensor(label, dtype=torch.long)
         return patch_feature, cluster_indices, label, case_id
 
-    def load_cluster_indices(self):
+    def load_cluster_indices(self) -> Dict[str: List[List[int]]]:
         cluster_indices = {}
         for case_id in self.indices:
             cluster_indices[case_id] = load_json(self.samples.at[case_id, 'clusters_json_filepath'])
@@ -121,7 +166,9 @@ class WSIWithCluster(WSIDataset):
 
 
 class ClusterFeatures(WSIWithCluster):
-    def __getitem__(self, index):
+    """A WSI Dataset, which patches sampled by cluster result. """
+
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor, str]:
         case_id = self.indices[index]
 
         if self.preload:
@@ -139,7 +186,8 @@ class ClusterFeatures(WSIWithCluster):
         label = torch.tensor(label, dtype=torch.long)
         return patch_feature, label, case_id
 
-    def sample_feat(self, patch_feature, cluster_indices=None):
+    def sample_feat(self, patch_feature: np.ndarray, cluster_indices: List[List[int]] = None) -> np.ndarray:
+        """Sample features by cluster indices. """
         if self.num_sample_patches is None:
             sample_ratio = 1.
         else:
@@ -160,7 +208,9 @@ class ClusterFeatures(WSIWithCluster):
 
 
 class WSIPhenotype(ClusterFeatures):
-    def __getitem__(self, index):
+    """A WSI Dataset, which patches sampled by cluster result and reshape it into a tenor list with shape like image. """
+
+    def __getitem__(self, index: int) -> Tuple[List[torch.Tensor], torch.Tensor, torch.Tensor, str]:
         case_id = self.indices[index]
 
         if self.preload:
@@ -177,7 +227,12 @@ class WSIPhenotype(ClusterFeatures):
         label = torch.tensor(label, dtype=torch.long)
         return feat, mask, label, case_id
 
-    def create_phenotype(self, patch_feature, cluster_indices):
+    def create_phenotype(self, patch_feature: np.ndarray, cluster_indices: List[List[int]]) -> \
+            Tuple[List[np.ndarray], np.ndarray]:
+        """
+        Returns a list of phenotypes and a mask, where each phenotype is a tensor composed of features of a cluster,
+        mask indicating whether a phenotype is not empty.
+        """
         if self.num_sample_patches is None:
             sample_ratio = 1.
         else:
@@ -198,17 +253,15 @@ class WSIPhenotype(ClusterFeatures):
                     indices = cluster_indices[cluster_idx]
                 if self.patch_random:
                     np.random.shuffle(indices)
-                # print(cluster_features_indices[cluster_idx])
                 cluster_feat = patch_feature[indices]
-            # print(f"cluster_feat: {cluster_feat.shape}")
             cluster_feat = np.swapaxes(cluster_feat, 1, 0)  # dim_features * num_features
-            # print(f"cluster_feat: {cluster_feat.shape}")
             cluster_feat = np.expand_dims(cluster_feat, 1)  # dim_features * 1 * num_features
             phenotype.append(cluster_feat)  # len == num_clusters
         return phenotype, mask
 
 
-def mixup(inputs, alpha):
+def mixup(inputs: torch.Tensor, alpha: Union[float, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """Mix-up a batch tentor. """
     batch_size = inputs.shape[0]
     lambda_ = alpha + torch.rand(size=(batch_size, 1), device=inputs.device) * (1 - alpha)
     rand_idx = torch.randperm(batch_size, device=inputs.device)
@@ -218,7 +271,11 @@ def mixup(inputs, alpha):
     return outputs, lambda_, rand_idx
 
 
-def get_feats(feat_list, clusters_list, action_sequence, feat_size=1024):
+def get_feats(feat_list: List[torch.Tensor],
+              clusters_list: List[List[List[int]]],
+              action_sequence: torch.Tensor,
+              feat_size: int = 1024) -> torch.Tensor:
+    """Construct a WSI-Fset mentioned in our paper. """
     batch_size = len(feat_list)
     device = action_sequence.device
     feats = []
@@ -249,8 +306,3 @@ def get_feats(feat_list, clusters_list, action_sequence, feat_size=1024):
         feats.append(per_feat)
     feats = torch.cat(feats, 0)
     return feats
-
-
-# Test -----------------------------------------------------------------------------------------------------------------
-if __name__ == '__main__':
-    pass
